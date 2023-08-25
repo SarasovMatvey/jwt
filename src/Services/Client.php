20ed7bee9d6c675a35eb97eca3f7b0813715857b
sarasov matvey <sarasovmatvej@gmail.com>
2023-08-25 11:56:34 +0600

    add base application layout, set up docker

---
+++ src/Services/Client.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+class Client implements Service {
+    public function run(): void {
+        
+    }
+}
---
+++ src/Services/Server.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services;
+
+class Server implements Service {
+    public function run(): void {
+
+    }
+}
