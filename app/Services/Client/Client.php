39f9cd8a8e818108a3214457c70c9b7bf20d9cbb
sarasov matvey <sarasovmatvej@gmail.com>
2023-08-26 15:43:03 +0600

    refactor client

--- app/Services/Client/Client.php
+++ app/Services/Client/Client.php
@@ -6,26 +6,45 @@ use App\Helpers\Profiler;
 use App\Services\ServiceInterface;
 use Firebase\JWT\JWT;
 use GuzzleHttp\Client as GuzzleClient;
+use Psr\Http\Message\ResponseInterface;
 
 class Client implements ServiceInterface
 {
+    protected GuzzleClient $guzzleClient;
+
+    /**
+     * @param GuzzleClient $guzzleClient
+     */
+    public function __construct(GuzzleClient $guzzleClient)
+    {
+        $this->guzzleClient = $guzzleClient;
+    }
+
     public function run(): void
     {
-        $token = JWT::encode([
-            'role' => 'admin'
-        ], $_ENV['JWT_PRIVATE_KEY'], 'RS256');
-
-        $client = new GuzzleClient();
+        $token = $this->generateJwt();
 
         $response = null;
-        $executionTime = Profiler::getExecutionTime(function () use (&$response, $client, $token) {
-            $response = $client->get('http://server/v1/foo', [
-                'headers' => [
-                    'Authorization' => $token
-                ]
-            ]);
+        $executionTime = Profiler::getExecutionTime(function () use (&$response, $token) {
+            $response = $this->makeRequest($token);
         });
 
         echo (new ResultFormatter())->format($executionTime, $response->getStatusCode());
+    }
+
+    protected function generateJwt(): string
+    {
+        return JWT::encode([
+            'role' => 'admin'
+        ], $_ENV['JWT_PRIVATE_KEY'], 'RS256');
+    }
+
+    protected function makeRequest($token): ResponseInterface
+    {
+        return $this->guzzleClient->get('http://server/v1/foo', [
+            'headers' => [
+                'Authorization' => $token
+            ]
+        ]);
     }
 }
--- app/Services/Client/ResultFormatter.php
+++ app/Services/Client/ResultFormatter.php
@@ -4,15 +4,18 @@ namespace App\Services\Client;
 
 class ResultFormatter
 {
+    protected const COLOR_GREEN = '\033[32m';
+    protected const COLOR_RED = '\033[31m';
+
     public function format(float $executionTime, int $statusCode): string
     {
         $executionTimeColor = match (true) {
-            $executionTime <= 100 => "\033[32m",
-            default => "\033[31m"
+            $executionTime <= 100 => self::COLOR_GREEN,
+            default => self::COLOR_RED
         };
         $statusCodeColor = match (true) {
-            $statusCode >= 200 && $statusCode <= 299 => "\033[32m",
-            default => "\033[31m"
+            $statusCode >= 200 && $statusCode <= 299 => self::COLOR_GREEN,
+            default => self::COLOR_RED
         };
 
         return
